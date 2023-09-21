f1a8c6e82c3ce9f740685386b1a5fadba76982e8
Magdalena Kloskowicz <magdalena.kloskowicz@starlingbank.com>
2023-09-21 14:24:22 +0100

    Update sortPeopleByDateOfBirth().

--- src/Main.java
+++ src/Main.java
@@ -8,22 +8,26 @@ public class Main {
 
   public static void main(String[] args) {
 
-    List <Person> people = List.of(
-    new Person("John", LocalDate.parse("1969-03-15"), "+447984356766", "john@gmail.com"),
-    new Person("Jane", LocalDate.parse("1998-04-09"), "+447220512328", "jane@gmail.com"),
-    new Person("Harry", LocalDate.parse("1980-09-25"), "+447220012555", "harry@gmail.com"),
-    new Person("Anne", LocalDate.parse("1969-03-15"), "+447220012222", "anne@gmail.com"),
-    new Person("Jack", LocalDate.parse("1996-08-20"), "+447220012098", "jack@gmail.com"));
-    
+    List<Person> people = List.of(
+        new Person("John", LocalDate.parse("1969-03-15"), "+447984356766", "john@gmail.com"),
+        new Person("Jane", LocalDate.parse("1998-04-09"), "+447220512328", "jane@gmail.com"),
+        new Person("Harry", LocalDate.parse("1980-09-25"), "+447220012555", "harry@gmail.com"),
+        new Person("Anne", LocalDate.parse("1969-03-15"), "+447220012222", "anne@gmail.com"),
+        new Person("Jack", LocalDate.parse("1996-08-20"), "+447220012098", "jack@gmail.com"));
 
-  LocalDate firstDate = LocalDate.parse("1980-01-01");
+    LocalDate firstDate = LocalDate.parse("1980-01-01");
 
-   for (Person i : people) {
-     if (WasBornAfter.wasBornAfter(i, firstDate)) {
-      System.out.println(i.getName());
-     }}
+    for (Person i : people) {
+      if (WasBornAfter.wasBornAfter(i, firstDate)) {
+        System.out.println(i.getName());
+      }
+    }
 
+   SortPeopleByDateOfBirth.sortPeopleByDateOfBirth(people);
 
+    for (Person person : people) {
+      System.out.println(person.getName() + " " + person.getDateOfBirth());
+    }
   }
 }
 
